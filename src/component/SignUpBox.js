fc1592a8470554e459e4cfaed5da6fe077723650
kwYoohae <gocksdl7979@gmail.com>
2022-11-14 10:58:50 +0900

    fix: Json으로 보내지 않은 부분 수정

--- src/component/SignUpBox.js
+++ src/component/SignUpBox.js
@@ -46,7 +46,11 @@ const SignUpBox = () => {
             return;
         }
 
-        axios.post("http://localhost:3001/check-id-duplicate", user_id)
+        const data = {
+            user_id: user_id
+        }
+
+        axios.post("http://localhost:3001/check-id-duplicate", data)
             .then((res) => {
                 if(res.data.duplicated === false) {
                     alert("아이디가 중복이 됩니다.");
@@ -63,7 +67,11 @@ const SignUpBox = () => {
             return;
         }
 
-        axios.post("http://localhost:3001/check-nickname-duplicate", nickname)
+        const data = {
+            nickname: nickname
+        };
+
+        axios.post("http://localhost:3001/check-nickname-duplicate", data)
             .then((res) => {
                 if(res.data.duplicated === false) {
                     alert("닉네임이 중복이 됩니다.");
